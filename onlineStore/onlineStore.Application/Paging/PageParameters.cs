75ceaa54d159f1827e92c6fd9397e2a06594750e
vitaliy Krasnorutskyi <vitaliikr@icloud.com>
2022-06-26 17:31:43 +0200

    Added Paging.

---
+++ onlineStore/onlineStore.Application/Paging/PageParameters.cs
@@ -0,0 +1,9 @@
+namespace onlineStore.Application.Paging
+{
+    public class PageParameters
+    {
+        public int PageSize { get; set; } = 10;
+
+        public int PageNumber { get; set; } = 1;
+    }
+}
