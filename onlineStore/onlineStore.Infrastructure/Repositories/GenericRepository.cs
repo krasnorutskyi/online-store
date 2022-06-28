d319e582ef427c1afa897bfa9d08bc3f35213c47
vitaliy Krasnorutskyi <vitaliikr@icloud.com>
2022-06-28 23:09:13 +0200

    Implemented PasswordHasher service

--- onlineStore/onlineStore.Infrastructure/Repositories/GenericRepository.cs
+++ onlineStore/onlineStore.Infrastructure/Repositories/GenericRepository.cs
@@ -5,103 +5,110 @@ using onlineStore.Application.Paging;
 using onlineStore.Core.Entities;
 using onlineStore.Infrastructure.ApplicationContext;
 
-namespace onlineStore.Infrastructure.Repositories;
-
-public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : EntityBase
+namespace onlineStore.Infrastructure.Repositories
 {
-    public GenericRepository(EFContext dbContext)
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : EntityBase
     {
-        this._db = dbContext;
-        this._table = dbContext.Set<TEntity>();
-    }
-
-    private DbSet<TEntity> _table;
-
-    private EFContext _db;
-    
-    public async Task AddAsync(TEntity item)
-    {
-        this._table.AddAsync(item);
-        this.SaveAsync();
-    }
-
-    public async Task UpdateAsync(TEntity item)
-    {
-        this._table.Update(item);
-        this.SaveAsync();
-    }
-
-    public async Task DeleteAsync(TEntity item)
-    {
-        this._table.Remove(item);
-        this.SaveAsync();
-    }
-
-    public async Task<TEntity> GetOneAsync(int id)
-    {
-        return await this._table.FirstOrDefaultAsync(entity => entity.Id == id);
-    }
-
-    public async Task<TEntity> GetOneAsync(int id, params Expression<Func<TEntity, object>>[] includeProperties)
-    {
-        var query =  this._table.Where(entity => entity.Id == id);
-        foreach (var property in includeProperties)
+        public GenericRepository(EFContext dbContext)
         {
-            query = query.Include(property);
+            this._db = dbContext;
+            this._table = dbContext.Set<TEntity>();
         }
 
-        return await query.FirstOrDefaultAsync(entity => entity.Id == id);
-    }
-    
-    public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
-    {
-        var query = this._table.Where(predicate);
-        foreach (var property in includeProperties)
+        private DbSet<TEntity> _table;
+
+        private EFContext _db;
+
+        public async Task AddAsync(TEntity item)
         {
-            query = query.Include(property);
+            this._table.AddAsync(item);
+            this.SaveAsync();
         }
 
-        return await query.ToListAsync<TEntity>();
-    }
-
-    public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters)
-    {
-        var items = this._table.AsNoTracking()
-            .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
-            .Take(pageParameters.PageSize);
-        var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
-        return pagedList;
-    }
-
-    public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, params Expression<Func<TEntity, object>>[] includeProperties)
-    {
-        var items = this._table.AsNoTracking()
-            .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
-            .Take(pageParameters.PageSize);
-        foreach (var property in includeProperties)
+        public async Task UpdateAsync(TEntity item)
         {
-            items = items.Include(property);
+            this._table.Update(item);
+            this.SaveAsync();
         }
-        var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
-        return pagedList;
-    }
 
-    public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
-    {
-        var items = this._table.AsNoTracking()
-            .Where(predicate)
-            .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
-            .Take(pageParameters.PageSize); ;
-        foreach (var property in includeProperties)
+        public async Task DeleteAsync(TEntity item)
         {
-            items = items.Include(property);
+            this._table.Remove(item);
+            this.SaveAsync();
         }
-        var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
-        return pagedList;
-    }
 
-    public async Task SaveAsync()
-    {
-        this._db.SaveChangesAsync();
+        public async Task<TEntity> GetOneAsync(int id)
+        {
+            return await this._table.FirstOrDefaultAsync(entity => entity.Id == id);
+        }
+
+        public async Task<TEntity> GetOneAsync(int id, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = this._table.Where(entity => entity.Id == id);
+            foreach (var property in includeProperties)
+            {
+                query = query.Include(property);
+            }
+
+            return await query.FirstOrDefaultAsync(entity => entity.Id == id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = this._table.Where(predicate);
+            foreach (var property in includeProperties)
+            {
+                query = query.Include(property);
+            }
+
+            return await query.ToListAsync<TEntity>();
+        }
+
+        public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters)
+        {
+            var items = this._table.AsNoTracking()
+                .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
+                .Take(pageParameters.PageSize);
+            var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
+            return pagedList;
+        }
+
+        public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var items = this._table.AsNoTracking()
+                .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
+                .Take(pageParameters.PageSize);
+            foreach (var property in includeProperties)
+            {
+                items = items.Include(property);
+            }
+
+            var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
+            return pagedList;
+        }
+
+        public async Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters,
+            Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var items = this._table.AsNoTracking()
+                .Where(predicate)
+                .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
+                .Take(pageParameters.PageSize);
+            ;
+            foreach (var property in includeProperties)
+            {
+                items = items.Include(property);
+            }
+
+            var pagedList = new PagedList<TEntity>(items, pageParameters, await items.CountAsync());
+            return pagedList;
+        }
+
+        public async Task SaveAsync()
+        {
+            this._db.SaveChangesAsync();
+        }
     }
 }
